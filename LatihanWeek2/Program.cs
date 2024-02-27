8cef2a3ddc17d0e1ae32b21b926ee5b8079f085d
unknown <farisazhar.0241@gmail.com>
2024-02-27 15:07:19 +0700

    tambah inisiasi

--- LatihanWeek2/Program.cs
+++ LatihanWeek2/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("write");
+Console.Write($"masukan nama : ");
+
+string input = Console.ReadLine();
+Console.WriteLine($"Halo {input} ! ");
 
 if (true)
 {
